3c88de4d970c46e977e68f791f879cdf04d34773
Braulio Betancourt <braulio.1796@gmail.com>
2017-10-17 13:13:31 -0500

    Implementación del proxy del cliente - en proceso

---
+++ src/knockKnock/ClientProxy.java
@@ -0,0 +1,53 @@
+package knockKnock;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import org.json.simple.JSONObject;
+
+public class ClientProxy {
+
+	public static final String REGISTRAR = "REGISTRAR";
+	
+	private String hostName, fromServer;
+	private int portNumber;
+	private Socket kkSocket;
+	private PrintWriter out;
+	private BufferedReader in;
+	private JSONObject json;
+	
+	public ClientProxy(String hostName, int portNumber) {
+		this.hostName = hostName;
+		this.portNumber = portNumber;
+	}
+	
+	public boolean getResponseFromServer() throws IOException {
+		return (this.fromServer = this.in.readLine()) != null;
+	}
+
+	@SuppressWarnings("unchecked")
+	public void register(String ip) {
+		this.json = new JSONObject();
+		this.json.put("SERVICIO", ClientProxy.REGISTRAR);
+		this.json.put("IP", ip);
+        this.out.println(this.json);
+	}
+
+	public void connect() {
+		try {
+			this.kkSocket = new Socket(this.hostName, this.portNumber);
+			this.out = new PrintWriter(this.kkSocket.getOutputStream(), true);
+			this.in = new BufferedReader(new InputStreamReader(this.kkSocket.getInputStream()));
+		} catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + this.hostName);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " + this.hostName);
+            System.exit(1);
+        }
+	}
+}
--- src/knockKnock/KnockKnockClient.java
+++ src/knockKnock/KnockKnockClient.java
@@ -31,57 +31,33 @@ package knockKnock;
  */ 
 
 import java.io.*;
-import java.net.*;
-
-import org.json.simple.JSONObject;
 
 public class KnockKnockClient {
+	private static final String IP = "192.168.0.1";
+	
     public static void main(String[] args) throws IOException {
-        
-        if (args.length != 2) {
+    	if (args.length != 2) {
             System.err.println(
                 "Usage: java EchoClient <host name> <port number>");
             System.exit(1);
         }
-
-        String hostName = args[0];
-        int portNumber = Integer.parseInt(args[1]);
-        
-        try (
-            Socket kkSocket = new Socket(hostName, portNumber);
-            PrintWriter out = new PrintWriter(kkSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(
-                new InputStreamReader(kkSocket.getInputStream()));
-        ) {
-//            BufferedReader stdIn =
-//                new BufferedReader(new InputStreamReader(System.in));
-            String fromServer;
-            String fromUser;
-            
-            //{"Servicio":"Registrar","ip":"192.158.1.0"}
-            JSONObject json = new JSONObject();
-            json.put("Servicio", "Registrar");
-            json.put("ip", "192.158.1.0");
-            //System.out.println(json);
-            out.println(json);
-            
-//            while ((fromServer = in.readLine()) != null) {
-//                System.out.println("Server: " + fromServer);
-//                if (fromServer.equals("Bye."))
-//                    break;
-//                fromUser = stdIn.readLine();
-//                if (fromUser != null) {
-//                    System.out.println("Client: " + fromUser);
-//                    out.println(json);
-//                }
-//            }
-        } catch (UnknownHostException e) {
-            System.err.println("Don't know about host " + hostName);
-            System.exit(1);
-        } catch (IOException e) {
-            System.err.println("Couldn't get I/O for the connection to " +
-                hostName);
-            System.exit(1);
-        }
+    	
+    	ClientProxy proxy = new ClientProxy(args[0], Integer.parseInt(args[1]));
+    	proxy.connect();
+    	
+    	BufferedReader stdIn;
+    	String fromUser;
+    	
+    	do {
+    		stdIn = new BufferedReader(new InputStreamReader(System.in));
+    		fromUser = stdIn.readLine();
+    		switch (fromUser) {
+    		case "Registrar":
+    		case "REGISTRAR":
+    		case "registrar":
+    			proxy.register(KnockKnockClient.IP);
+    			break;
+    		}
+    	} while (proxy.getResponseFromServer());
     }
 }
